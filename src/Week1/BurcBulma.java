1a2b3517444877500ea8f0f01cc21d4c77db6421
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2023-09-13 18:50:07 +0300

    Burç Bulma Pratiği yüklendi

--- src/Week1/BurcBulma.java
+++ src/Week1/BurcBulma.java
@@ -18,6 +18,17 @@ public class BurcBulma {
 
         //Doğduğu aya ve güne göre if else bloklarıyla burcunu belirleyip kullanıcıya yansıtıyoruz.
 
+        //Koç Burcu. 21 Mart- 20 Nisan.
+        //Boğa Burcu. 21 Nisan – 20 Mayıs.
+        //İkizler Burcu. 21 Mayıs – 21 Haziran.
+        //Yengeç Burcu. 22 Haziran – 22 Temmuz.
+        //Aslan Burcu. 23 Temmuz – 23 Ağustos.
+        //Başak Burcu. 24 Ağustos – 23 Eylül.
+        //Terazi Burcu. 24 Eylül – 23 Ekim.
+        //Akrep Burcu. 24 Ekim – 22 Kasım.
+        //Yay Burcu. 23 Kasım - 21 Aralık.
+        //Kova Burcu. 21 Ocak - 19 Şubat.
+        //Balık Burcu. 20 Şubat - 20 Mart.
         if (month==1)
         {
             if(day>=1 && day<=31)
