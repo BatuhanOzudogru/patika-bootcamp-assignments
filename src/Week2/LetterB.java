1936c529303f59de85d0dcbe521b22a99818b0a7
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2023-09-21 15:15:09 +0300

    letterB added.

---
+++ src/Week2/LetterB.java
@@ -0,0 +1,27 @@
+package Week2;
+
+public class LetterB {
+        public static void main(String[] args) {
+        String[][] letterB= new String[7][4];
+
+            for(int i=0; i<letterB.length;i++) {
+                for (int j = 0; j < letterB[i].length; j++) {
+                    if (i == 0 || i == 3 || i == 6) {
+                        letterB[i][j] = " * ";
+                    } else if (j == 0 || j == 3) {
+                        letterB[i][j] = " * ";
+
+                    } else {
+                        letterB[i][j] = "   ";
+                    }
+                }
+            }
+            for(String[] row : letterB){
+                for(String col : row){
+                    System.out.print(col);
+                }
+                System.out.println();
+            }
+        }
+}
+
