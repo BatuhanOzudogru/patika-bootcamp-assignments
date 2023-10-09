e481c554651eeaa993d03995e0a3168552ff3b5f
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2023-10-09 15:40:52 +0300

    Fix try-catch

--- src/Week5/Try_Catch.java
+++ src/Week5/Try_Catch.java
@@ -3,8 +3,10 @@ package Week5;
 import java.util.Scanner;
 
 public class Try_Catch {
-    public static void printIndex(int[]arr,int selectIndex){
-
+    public static void printIndex(int[]arr,int selectIndex) throws  ArrayIndexOutOfBoundsException{
+        if(selectIndex>arr.length-1||selectIndex<0){
+            throw new ArrayIndexOutOfBoundsException("Hatalı indis numarası girdiniz. İndis numarası 0-9 arasında olmalıdır");
+        }
         System.out.println(arr[selectIndex]);
     }
 
@@ -18,8 +20,8 @@ public class Try_Catch {
         try{
            printIndex(arr,selectIndex);
         }catch (ArrayIndexOutOfBoundsException e){
-            e.printStackTrace();
-            System.out.println("Hatalı indis numarası girdiniz. İndis numarası 0-9 arasında olmalıdır");
+            System.out.println(e.getMessage());
+            System.out.println("Error : Dizi sınırlarının dışına çıkıldı");
         }
         System.out.println("Program bitti..");
 
