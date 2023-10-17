7972b7a936720e0499f5117790e5278a05aa0c3e
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2023-10-17 15:37:40 +0300

    Add comment

--- src/Week6/JDBC.java
+++ src/Week6/JDBC.java
@@ -11,19 +11,26 @@ public class JDBC {
 
         Connection connect = null;
         Statement st = null;
+        // SQL query to retrieve data from the database
         String sql = "SELECT * FROM employees";
         try {
+            // Establish a database connection
             connect = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            // Create a statement for executing SQL queries
             st = connect.createStatement();
+            // Execute the SQL query and get the result set
             ResultSet rs = st.executeQuery(sql);
+            // Iterate through the result set and print employee details
             while (rs.next()) {
                 System.out.println("Id : " + rs.getInt("employee_id") + " Name : " + rs.getString("employee_name") +
                         " Position : " + rs.getString("employee_position") + " Salary : " + rs.getInt("employee_salary"));
             }
 
-
+            // Close the database connection
             connect.close();
         } catch (SQLException e) {
+            // Handle any SQLException that might occur during database operations
+            // and throw a RuntimeException with the original exception for debugging purposes
             throw new RuntimeException(e);
         }
     }
