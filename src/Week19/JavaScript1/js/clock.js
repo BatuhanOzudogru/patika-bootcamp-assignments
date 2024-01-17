7b2cd160d102a29e7e8fe0061df29ee20f325da3
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-01-18 01:31:50 +0300

    Add JavaScript Assignment1

---
+++ src/Week19/JavaScript1/js/clock.js
@@ -0,0 +1,26 @@
+let namePrompt= prompt("Please enter your name");
+
+let nameDOM = document.querySelector('#myName');
+
+nameDOM.innerHTML = namePrompt;
+
+function showTime() {
+    const date = new Date();
+    let day = getDayName();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('myClock').innerHTML =  hour + ":" + minute + ":" + second + " " + day;
+    setTimeout(showTime, 500);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  
+    return i;
+  }
+
+  function getDayName(date = new Date(), locale = 'en-US') {
+    return date.toLocaleDateString(locale, {weekday: 'long'});
+  }
