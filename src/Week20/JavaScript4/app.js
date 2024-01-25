6c999543bd2e0d9c50056f310e0f76fcf230ad39
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-01-25 22:53:30 +0300

    Add JavaScript Assignment4

--- src/Week20/JavaScript4/app.js
+++ src/Week20/JavaScript4/app.js
@@ -105,9 +105,11 @@ const categoryList = () => {
   btnContainer.innerHTML = categoryBtns;
   const filterBtns = document.querySelectorAll(".btn-item");
 
+  //filter menu
   filterBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
       const category = e.currentTarget.dataset.id;
+      console.log(category);
       const menuCategory = menu.filter((menuItem) => {
         if (menuItem.category === category) {
           return menuItem;
@@ -122,6 +124,32 @@ const categoryList = () => {
   });
 };
 
+const menuList = (menuItems) => {
+  let displayMenu = menuItems.map((item) => {
+    return `<div class="menu-items col-lg-6 col-sm-12">
+            <img
+              src=${item.img}
+              alt=${item.title}
+              class="photo"
+            />
+            <div class="menu-info">
+              <div class="menu-title">
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+              </div>
+              <div class="menu-text">
+                ${item.desc}
+              </div>
+            </div>
+          </div>
+    `;
+  });
+  displayMenu = displayMenu.join("");
+  section.innerHTML = displayMenu;
+};
+
+menuList(menu);
+categoryList();
 
 
 
