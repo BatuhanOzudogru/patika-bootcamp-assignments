4aced4c5f78fbf1efd4ba1222f50c5963fe4e5e5
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-03-25 20:11:10 +0300

    Add css

--- Week23/StarWarsApp/src/Pages/Details.jsx
+++ Week23/StarWarsApp/src/Pages/Details.jsx
@@ -9,6 +9,7 @@ import Typography from "@mui/material/Typography";
 import { IconButton } from "@mui/material";
 import { CardActionArea } from "@mui/material";
 import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import "./Details.css";
 
 function Details() {
   const { id } = useParams();
@@ -27,17 +28,18 @@ function Details() {
   }
 
   return (
-    <>
-      <div>
-        <a href={`/`}>
+    <div className="container">
+      <div className="back-button">
+        <a href="/">
           <IconButton>
             <ArrowBackRoundedIcon />
           </IconButton>
         </a>
-
-        <Card sx={{ maxWidth: 345 }}>
+      </div>
+      <div className="card-container">
+        <Card sx={{ maxWidth: 345, borderRadius: 7, backgroundColor : "#111423", color : "#c0c2d9"}}>
           <CardActionArea>
-            <Typography gutterBottom variant="h5" component="div">
+            <Typography gutterBottom variant="h5" component="div" sx={{textAlign: "center"}}>
               {starship.name}
               <hr />
             </Typography>
@@ -46,29 +48,30 @@ function Details() {
               height="140"
               image="https://content.invisioncic.com/x311865/monthly_2022_06/HWC_Article_Header_HHR14_SWStarships_v1a.jpg.fbaf20672ce772a26f304014c5b99c9e.jpg"
               alt={starship.name}
+              sx={{ borderRadius: 7 }}
             />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                Model : {starship.model}
+            <CardContent sx={{ textAlign: "center" }}>
+              <Typography variant="body2" color="text.secondary" sx={{color : "#c0c2d9"}}>
+                <strong>Model:</strong> {starship.model}
                 <br />
-                Hyperdrive Rating : {starship.hyperdrive_rating}
+                <strong>Hyperdrive Rating :</strong> {starship.hyperdrive_rating}
                 <br />
-                Passengers : {starship.passengers}
+                <strong>Passengers :</strong> {starship.passengers}
                 <br />
-                Max Atmosphering Speed : {starship.max_atmosphering_speed}
+                <strong>Max Atmosphering Speed :</strong> {starship.max_atmosphering_speed}
                 <br />
-                Manufacturer : {starship.manufacturer}
+                <strong>Manufacturer :</strong> {starship.manufacturer}
                 <br />
-                Crew : {starship.crew}
+                <strong>Crew : </strong> {starship.crew}
                 <br />
-                Cargo Capacity {starship.cargo_capacity}
+                <strong>Cargo Capacity </strong>{starship.cargo_capacity}
                 <br />
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
       </div>
-    </>
+    </div>
   );
 }
 
--- Week23/StarWarsApp/src/Pages/Starship.jsx
+++ Week23/StarWarsApp/src/Pages/Starship.jsx
@@ -9,12 +9,14 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
+import "./Starship.css";
 
 function Starship() {
   const [starships, setStarships] = useState([]);
   const [reload, setReload] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const [loadedStarships, setLoadedStarships] = useState(5);
+  const [cardsMarginTop, setCardsMarginTop] = useState("10em"); // State for controlling margin-top of .cards
 
   useEffect(() => {
     getStarships().then((data) => {
@@ -24,15 +26,21 @@ function Starship() {
   }, [reload]);
 
   const handleSearch = () => {
-    const results = starships.filter((item) =>
-      item.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setStarships(results);
-    setReload(false);
+    if (searchTerm.trim() === "") {
+      setStarships(starships);
+    } else {
+      const results = starships.filter((item) =>
+        item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.model.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setStarships(results);
+      setReload(false);
+    }
   };
 
   const loadMoreStarships = () => {
     setLoadedStarships((prevCount) => prevCount + 5);
+    setCardsMarginTop("3em"); // Update margin-top when loading more starships
   };
 
   if (reload || starships.length === 0) {
@@ -42,12 +50,16 @@ function Starship() {
   return (
     <>
       <div>
-        <h1>Starships</h1>
+        <header>
+          <img src="https://www.cdnlogo.com/logos/s/43/star-wars.svg" alt="" />
+        </header>
 
         <div className="search">
+          <label htmlFor="">Name / Model</label>
           <input
             type="text"
             name="search"
+            placeholder="Name / Model"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
@@ -58,41 +70,55 @@ function Starship() {
           >
             Filter
           </Button>
+          <br />
+          <hr className="search-divider" />
         </div>
-
-        {starships.slice(0, loadedStarships).map((starship) => (
-          <a
-            href={`/starships/${starship.url.split("/")[5]}`}
-            key={starship.url.split("/")[5]}
-          >
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="https://content.invisioncic.com/x311865/monthly_2022_06/HWC_Article_Header_HHR14_SWStarships_v1a.jpg.fbaf20672ce772a26f304014c5b99c9e.jpg"
-                  alt={starship.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {starship.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Model : {starship.model}
-                    <br />
-                    Hyperdrive Rating : {starship.hyperdrive_rating}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </a>
-        ))}
+  
+        <div className="cards" style={{ marginTop: cardsMarginTop }}> {/* Dynamically set margin-top */}
+          {starships.slice(0, loadedStarships).map((starship) => (
+            <>
+              <div className="card">
+                <a
+                  href={`/starships/${starship.url.split("/")[5]}`}
+                  key={starship.url.split("/")[5]}
+                >
+                  <Card sx={{ maxWidth: 345, borderRadius: 7, backgroundColor : "#111423", color : "#c0c2d9"}}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image="https://content.invisioncic.com/x311865/monthly_2022_06/HWC_Article_Header_HHR14_SWStarships_v1a.jpg.fbaf20672ce772a26f304014c5b99c9e.jpg"
+                        alt={starship.name}
+                        sx={{ borderRadius: 7 }}
+                      />
+                      <CardContent sx={{ textAlign: "center" }}>
+                        <Typography gutterBottom variant="h5" component="div">
+                          <strong>{starship.name}</strong>
+                        </Typography>
+                        <Typography variant="body2" color="text.primary" sx={{color : "#c0c2d9"}}>
+                          <strong>Model:</strong> {starship.model}
+                          <br />
+                          <strong>Hyperdrive Rating:</strong> {starship.hyperdrive_rating}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </a>
+              </div>
+            </>
+          ))}
+       
+        </div>
+      
+        <div className="more-btn">
         {starships.length > loadedStarships && (
-          <Button variant="contained" onClick={loadMoreStarships}>
+          <Button variant="contained" sx={{backgroundColor: '#134e70'}} onClick={loadMoreStarships}>
             Load More
           </Button>
         )}
+        </div>
       </div>
+    
     </>
   );
 }
